eacda7603defc1597a8fbe6f2eb42fb518159033
Victor Cardona <vicardona7@gmail.com>
2022-01-11 18:51:50 +0100

    added roles to user dto

--- src/modules/user/dto/read-user.dto.ts
+++ src/modules/user/dto/read-user.dto.ts
@@ -1,17 +1,27 @@
-import { Type } from "class-transformer";
+import { Exclude, Expose, Type } from "class-transformer";
 import { IsEmail, IsNumber } from "class-validator";
-import { ReadUserDetailsDto } from "./read-user-details.dto";
+import { ReadUserDetailsDto } from "./index";
+import { ReadRoleDto } from "../../role/dto/index.dto";
 
+@Exclude()
 export class ReadUserDto {
+    @Expose()
     @IsNumber()
     readonly id: number;
 
+    @Expose()
     @IsEmail()
     readonly email: string;
     
+    @Expose()
     @IsEmail()
     readonly username: string;
 
+    @Expose()
     @Type(type => ReadUserDetailsDto)
     readonly details: ReadUserDetailsDto
+    
+    @Expose()
+    @Type(type => ReadRoleDto)
+    readonly roles: ReadRoleDto
 }
